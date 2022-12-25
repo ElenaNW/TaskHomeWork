95d995b640b44bafcb3a6d036e00375a5ef928a8
Elena <marenina.e@yandex.com>
2022-12-26 00:32:30 +0300

    Задача 13 выполнена

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,5 +5,31 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-int number = new Random.Next(1, 2000);
+int number = new Random().Next(1, 24000);
+Console.WriteLine($"Случайное число: {number}");
+if (number >= 100)
+{
+    if (number >= 100 && number <= 999)
+    {
+        Console.WriteLine($"Третья цифра числа -> {RemainsNumber(number)}");
+    }
+    else
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        Console.WriteLine($"Третья цифра числа -> {RemainsNumber(number)}");
+    }
+}
+else
+{
+    Console.WriteLine("Третьей цифры в числе нет");
+}
 
+
+
+int RemainsNumber(int num)
+{
+    return number % 10;
+}
