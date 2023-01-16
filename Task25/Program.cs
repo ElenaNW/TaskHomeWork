6655498f5f589397b232e44a7de425fe86130804
Elena <marenina.e@yandex.com>
2023-01-16 18:26:22 +0300

    Задача 25 выполнена

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Write("Введите целое число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = NumberDegree(numberA, numberB);
+Console.Write($"{numberA},{numberB} -> {result}");
+
+if (numberB > 0)
+{
+    NumberDegree(numberA, numberB);
+}
+else Console.Write("Число B не является натуральным");
+
+
+int NumberDegree(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        result = result * numA; 
+    }
+    return result;
+}
