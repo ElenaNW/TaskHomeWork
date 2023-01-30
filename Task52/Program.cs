fe21a878da07283072650904a79249ec82e47cf5
Elena <marenina.e@yandex.com>
2023-01-30 09:50:04 +0300

    Задача 52 выполнена

---
+++ Task52/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2D);
+double[] array = ArithmeticMeanColumns(array2D);
+Console.Write("Среднее арифметическое каждого столбца: ");
+PrintArray(array);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] ArithmeticMeanColumns(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+            double mean = sum / matrix.GetLength(0);
+            array[j] = Math.Round(mean, 2);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+}
